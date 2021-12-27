7fe374ffaf7356665c3dc01cb20a874bb07d6395
Bhaswati Roy <roybhaswati24@gmail.com>
2021-12-27 22:32:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from streamlit.proto.Button_pb2 import Button
+
+rad=st.sidebar.radio("Navigation Menu",["Home","Covid-19","Diabetes","Heart Disease"])
+
+#Home Page
+if rad=="Home":
+    st.title("Medical Predictions App")
+    st.image("Medical Prediction Home Page.jpg")
+    st.text("The Following Disease Predictions Are Available ->")
+    st.text("1. Covid-19 Infection Predictions")
+    st.text("2. Diabetes Predictions")
+    st.text("3. Heart Disease Predictions")
+
+#Covid-19 Prediction
+df1=pd.read_csv("Covid-19 Predictions.csv")
+x1=df1.drop("Infected with Covid19",axis=1)
+x1=np.array(x1)
+y1=pd.DataFrame(df1["Infected with Covid19"])
+y1=np.array(y1)
+x1_train,x1_test,y1_train,y1_test=train_test_split(x1,y1,test_size=0.2,random_state=0)
+model1=RandomForestClassifier()
+model1.fit(x1_train,y1_train)
+
+#Covid-19 Page
+if rad=="Covid-19":
+    st.header("Know If You Are Affected By Covid-19")
+    st.write("All The Values Should Be In Range Mentioned")
+    drycough=st.number_input("Rate Of Dry Cough (0-20)",min_value=0,max_value=20,step=1)
+    fever=st.number_input("Rate Of Fever (0-20)",min_value=0,max_value=20,step=1)
+    sorethroat=st.number_input("Rate Of Sore Throat (0-20)",min_value=0,max_value=20,step=1)
+    breathingprob=st.number_input("Rate Of Breathing Problem (0-20)",min_value=0,max_value=20,step=1)
+    prediction1=model1.predict([[drycough,fever,sorethroat,breathingprob]])[0]
+
+    if st.button("Predict"):
+        if prediction1=="Yes":
+            st.warning("You Might Be Affected By Covid-19")
+        elif prediction1=="No":
+            st.success("You Are Safe")
+
+#Diabetes Prediction
+df2=pd.read_csv("Diabetes Predictions.csv")
+x2=df2.iloc[:,[1,4,5,7]].values
+x2=np.array(x2)
+y2=y2=df2.iloc[:,[-1]].values
+y2=np.array(y2)
+x2_train,x2_test,y2_train,y2_test=train_test_split(x2,y2,test_size=0.2,random_state=0)
+model2=RandomForestClassifier()
+model2.fit(x2_train,y2_train)
+
+#Diabetes Page
+if rad=="Diabetes":
+    st.header("Know If You Are Affected By Diabetes")
+    st.write("All The Values Should Be In Range Mentioned")
+    glucose=st.number_input("Enter Your Glucose Level (0-200)",min_value=0,max_value=200,step=1)
+    insulin=st.number_input("Enter Your Insulin Level In Body (0-850)",min_value=0,max_value=850,step=1)
+    bmi=st.number_input("Enter Your Body Mass Index/BMI Value (0-70)",min_value=0,max_value=70,step=1)
+    age=st.number_input("Enter Your Age (20-80)",min_value=20,max_value=80,step=1)
+    prediction2=model2.predict([[glucose,insulin,bmi,age]])[0]
+
+    if st.button("Predict"):
+        if prediction2==1:
+            st.warning("You Might Be Affected By Diabetes")
+        elif prediction2==0:
+            st.success("You Are Safe")
+
+#Heart Disease Prediction
+df3=pd.read_csv("Heart Disease Predictions.csv")
+x3=df3.iloc[:,[2,3,4,7]].values
+x3=np.array(x3)
+y3=y3=df3.iloc[:,[-1]].values
+y3=np.array(y3)
+x3_train,x3_test,y3_train,y3_test=train_test_split(x3,y3,test_size=0.2,random_state=0)
+model3=RandomForestClassifier()
+model3.fit(x3_train,y3_train)
+
+#Diabetes Page
+if rad=="Heart Disease":
+    st.header("Know If You Are Affected By Heart Disease")
+    st.write("All The Values Should Be In Range Mentioned")
+    chestpain=st.number_input("Rate Your Chest Pain (1-4)",min_value=1,max_value=4,step=1)
+    bp=st.number_input("Enter Your Blood Pressure Rate (95-200)",min_value=95,max_value=200,step=1)
+    cholestrol=st.number_input("Enter Your Cholestrol Level Value (125-565)",min_value=125,max_value=565,step=1)
+    maxhr=st.number_input("Enter You Maximum Heart Rate",min_value=70,max_value=200,step=1)
+    prediction3=model3.predict([[chestpain,bp,cholestrol,maxhr]])[0]
+
+    if st.button("Predict"):
+        if str(prediction3)=="Presence":
+            st.warning("You Might Be Affected By Diabetes")
+        elif str(prediction3)=="Absence":
+            st.success("You Are Safe")
